8d3eae0bc336b946270295b92eb3445f9a7ec65d
Tobias Windisch <tobias.windisch@hs-kempten.de>
2024-12-05 09:42:33 +0100

    update setup.py
    Signed-off-by: Tobias Windisch <tobias.windisch@hs-kempten.de>

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
+with open('VERSION') as f:
+    version = f.read().strip()
+
 setup(
     name='driftbench',
-    version='1.0',
-    packages=['driftbench', 'driftbench.data_generation'],
+    version=version,
+    packages=['driftbench'],
     url='',
-    license='',
+    license="Apache-2.0",
     author='Edgar Wolf',
     author_email='edgar.wolf@hs-kempten.de',
-    description=''
+    description='A package to benchmark process drift detection'
 )
