cff792087d76c4d86559dea8947df0eb59f76757
Tobias Windisch <tobias.windisch@hs-kempten.de>
2024-12-05 10:06:59 +0100

    update long desription
    Signed-off-by: Tobias Windisch <tobias.windisch@hs-kempten.de>

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from pathlib import Path
 
 with open('VERSION') as f:
     version = f.read().strip()
@@ -11,6 +12,6 @@ setup(
     author='Edgar Wolf',
     author_email='edgar.wolf@hs-kempten.de',
     description='A package to benchmark process drift detection',
-    long_description='A package to benchmark process drift detection',
+    long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type='text/markdown',
 )
